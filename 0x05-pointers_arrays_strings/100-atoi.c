064c41d12d8a9cfb6473e64cec2f5ae80c6d7282
smilealien05 <ismailstarallah0@gmail.com>
2023-08-21 17:47:50 -0700

    correct 100 file second time

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,35 +9,36 @@
 int _atoi(char *s)
 {
 	int i = 0;
-	int sign = 0;
-	int result = 0;
+	int d = 0;
+	int n = 0;
+	int len = 0;
+	int f = 0;
+	int digit = 0;
 
-	for (i = 0; s[i] >= '9' || s[i] <= '0'; i++)
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
-			++sign;
-		if (s[i] <= '9' && s[i] >= '0')
-			break;
-	}
-	if (sign % 2  == 1)
-	{
-		sign = -1;
-	} else
-	{
-		sign = 1;
-	}
-	while (s[i] != '\0')
-	{
-		if (s[i] <= '9' && s[i] >= '0')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			result = result * 10 + (s[i] - '0');	
-		} else
-		{
-			break;
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
 		}
 		i++;
 	}
-	if (result == 0)
+
+	if (f == 0)
 		return (0);
-	return (sign * result);
+
+	return (n);
 }
