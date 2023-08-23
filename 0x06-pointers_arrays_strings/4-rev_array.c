72af9a682badcfa7410b630e16b4eb96e9fbbbb7
smilealien05 <ismailstarallah0@gmail.com>
2023-08-23 11:23:57 -0700

    a function reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+ * reverse_array - function reverse array
+ * @a: array 
+ * @n: intiger that mean size of case in array
+ * Return: void
+*/
+void reverse_array(int *a, int n)
+{
+int start = 0;
+int i = 0;
+n--;
+while (start < n)
+{
+i = a[start];
+a[start] = a[n];
+a[n] = i;
+n--;
+start++;
+}
+}
