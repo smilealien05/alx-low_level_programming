29d7f35f7d6823c206e17048924074053910bb35
smilealien05 <ismailstarallah0@gmail.com>
2023-08-23 10:53:02 -0700

    second correct

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,8 +8,14 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-int result;
-
-result = s1[0] - s2[0];
-return (result);
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
