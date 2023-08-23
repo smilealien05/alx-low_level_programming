e37459383e3e93332206e9975f130b4b72355ff2
smilealien05 <ismailstarallah0@gmail.com>
2023-08-23 10:40:43 -0700

    a function that compare between two str

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * _strcmp -  function that compares two strings.
+ * @s1: a string
+ * @s2: a string
+ * Return: intiger
+*/
+int _strcmp(char *s1, char *s2)
+{
+int n = 0;
+int result;
+result = s1[n] - s2[n];
+return (result);
+}
