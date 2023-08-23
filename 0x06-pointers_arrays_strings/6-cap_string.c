3a2f80e5bb36835cef5977a964d8b6a450c6535b
smilealien05 <ismailstarallah0@gmail.com>
2023-08-23 16:24:36 -0700

    correct 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,6 @@ char *cap_string(char *str)
 int i;
 int j;
 char separators[] = " \t\n,;.!?\"(){}";
-
 if (str[0] <= 'z' && str[0] >= 'a')
 {
 str[0] = str[0] - 32;
@@ -20,9 +19,12 @@ for (i = 1; str[i] != '\0'; i++)
 {
 for (j = 0; separators[j] != '\0'; j++)
 {
+if (separators[j] == str[i])
+{
 if (str[i + 1] <= 'z' && str[i + 1] >= 'a')
 {
 str[i + 1] = str[i + 1] - 32;
+}
 }
 }
 }
