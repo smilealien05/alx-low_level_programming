8e8e6be5681c06377d231ddb6b012b7f91824455
smilealien05 <ismailstarallah0@gmail.com>
2023-08-24 17:41:49 -0700

    the last commit of this day

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,6 +3,12 @@
 
 int _putchar(char);
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void print_number(int n);
+
+
 
 
 #endif
