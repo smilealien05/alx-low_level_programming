7b942635da4f74958793817cfdfa2fb778bea161
smilealien05 <ismailstarallah0@gmail.com>
2023-09-07 15:19:31 -0700

    finished tasks

---
+++ 0x02-functions_nested_loops/test_file.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+
+/**
+* print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+* followed by a new line
+*/
+void print_test(void)
+{
+	for (int a = 0; a < 10; a++)
+		print_alphabet();
+}
