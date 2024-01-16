bb071ab0fdb8b56ed272c22c1a41c3d92ffc4b59
smilealien05 <ismailstarallah0@gmail.com>
2024-01-16 08:25:00 -0800

    correct a small error in 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,11 +13,12 @@ char *str_concat(char *s1, char *s2)
 	char *concat;
 	int i, ii;
 
-	if (s1 == '\0')
+	if (s1 == NULL)
 		s1 = "";
-	if (s2 == '\0')
+	if (s2 == NULL)
 		s2 = "";
-	i = ii = 0;
+	i = 0;
+	ii = 0;
 	while (s1[i] != '\0')
 		i++;
 	while (s2[ii] != '\0')
@@ -25,7 +26,8 @@ char *str_concat(char *s1, char *s2)
 	concat = malloc(sizeof(char) * (i + ii + 1));
 	if (concat == NULL)
 		return (NULL);
-	i = ii = 0;
+	i = 0;
+	ii = 0;
 	while (s1[i] != '\0')
 	{
 		concat[i] = s1[i];
