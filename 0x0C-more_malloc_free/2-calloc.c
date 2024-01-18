840ae13b40d6a7de4b65e33f1fe64d175cc38228
smilealien05 <ismailstarallah0@gmail.com>
2024-01-18 14:29:57 -0800

    correct

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -7,7 +7,7 @@
 * @size: allocate element of size bytes
 * Return: pointer to the allocated memory.
 */
-void * _calloc(unsigned int nmemb, unsigned int size)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
 char *a;
 unsigned int b;
